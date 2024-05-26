cc11b6614e39caacca6ed970df4cf8da00c07975
Alatise <fayobio17@gmail.com>
2024-05-26 14:27:18 +0100

    changes made to header on small screen

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <div className="flex justify-between items-center px-16 my-5">
+      <div className="flex flex-col sm:flex-row justify-between items-center px-4 sm:px-16 my-5">
         <div></div>
-        <div className="flex gap-4 items-center">
+        <div className="flex flex-col sm:flex-row gap-4 items-center">
           <p>
             <span className="font-bold">Phone:</span> (+443) 200 3107
           </p>
-          <span className="border-r h-full mx-2"></span>
+          <span className="hidden sm:block border-r h-full mx-2"></span>
           <p>
             <span className="font-bold">Support:</span> placeholder@pbds.com
           </p>
